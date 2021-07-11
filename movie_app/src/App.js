148edcb572db31916d554a3bb718e40b2580ffe9
sangmin song <ssmin940606@gmail.com>
2021-07-11 23:56:01 +0900

    :tada: start project

---
+++ movie_app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class App extends React.Component{
+  state = {
+    isLoading: true
+  };
+  render(){
+    const isLoading = this.state;
+    return (
+      <div>
+        { isLoading ? "Loading" : "we are ready"}
+      </div>
+    )
+  }
+}
+
+export default App;
