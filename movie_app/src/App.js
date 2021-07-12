96823bd8363c4d07a902c92b0dac0a11c9ff2ae9
sangmin song <ssmin940606@gmail.com>
2021-07-12 17:38:58 +0900

    feat: add setTimeout for page loading - add setTimeout function to show the sign for page loaded

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
 
 class App extends React.Component{
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
   };
+  componentDidMount() {
+   setTimeout(() => {
+     this.setState({ isLoading: false })
+   }, 3000); 
+  }
   render(){
-    const isLoading = this.state;
+    const { isLoading } = this.state;
     return (
       <div>
         { isLoading ? "Loading" : "we are ready"}
       </div>
     )
-  }
+  };
+
 }
 
 export default App;
